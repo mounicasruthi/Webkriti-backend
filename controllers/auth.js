649c718967a4c49ee9564e0e8a589971884a5d41
disha9112 <disha9112@gmail.com>
2021-07-01 14:31:42 +0530

    finished sign up and sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,75 +1,128 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const client = require("../configs/db");
 
-const tempData = [
-  {
-    name: "Mounica",
-    email: "mounica@gmail.com",
-    password: "mounicaweb",
-  },
-  {
-    name: "Arushi",
-    email: "arushi@gmail.com",
-    password: "arushiweb",
-  },
-  {
-    name: "Disha",
-    email: "disha@gmail.com",
-    password: "dishaweb",
-  },
-];
+// const tempData = [
+//   {
+//     name: "Mounica",
+//     email: "mounica@gmail.com",
+//     password: "mounicaweb",
+//   },
+//   {
+//     name: "Arushi",
+//     email: "arushi@gmail.com",
+//     password: "arushiweb",
+//   },
+//   {
+//     name: "Disha",
+//     email: "disha@gmail.com",
+//     password: "dishaweb",
+//   },
+// ];
 
 exports.signup = (req, res) => {
-  // {
-  //     name: "Mounica",
-  //     email: "mounica@gmail.com",
-  //     password: "mounicaweb"
-  // }
-
   const { name, email, password } = req.body;
-  console.log(name, email, password);
 
-  //Check if the user already exists in the database or not
-  const isValid = tempData.findIndex((e) => e.email === email);
+  // const isValid = tempData.findIndex((e) => e.email === email);
 
-  if (isValid != -1) {
-    res.status(400).json({
-      error: "User already exists",
-    });
-  }
+  client
+  .query(`SELECT * FROM users where email = '${email}';`)
+  .then((data) => {
+    isValid = data.rows;
 
-  //Hash password
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) {
+    if (isValid.length != 0) {
       res.status(400).json({
-        error: "Internal server error",
+        error: "User already exists",
       });
-    }
-    const user = {
-      name,
-      email,
-      password: hash,
-    };
-    tempData.push(user);
+    }else{
+  
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        res.status(400).json({
+          error: "Internal server error",
+        });
+      }
+      const user = {
+        name,
+        email,
+        password: hash,
+      };
 
-    console.log(tempData);
-    res.status(200).json({
-      message: "User added successfully",
-      token: token,
+      // tempData.push(user);
+      client
+      .query(`INSERT INTO users (name, email, password) VALUES ('${user.name}', '${user.email}', '${user.password}');`
+      )
+      .then((data) => {
+        const token = jwt.sign(
+          {
+            email: email,
+          },
+          process.env.secret_key
+        );
+        res.status(200).json({
+          message: "User added successfully",
+          token: token,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: "Database error occurred",
+        });
+      
     });
   });
-
-  //Generate token
-  const token = jwt.sign(
-    {
-      email: email,
-    },
-    process.env.secret_key
-  );
-
-  //Send response to user along with the token
+  }
+  })
+  .catch((err) => {
+    res.status(500).json({
+      error: "Database error occurred",
+    });
+  });
 };
-
+  
 exports.signin = (req, res) => {
-    
+  const { email, password } = req.body;
+
+  // const isValid = tempData.findIndex((e) => e.email === email);
+
+  client
+  .query(`SELECT * FROM users where email = '${email}';`)
+  .then((data) => {
+    userData = data.rows;
+
+    if (userData.length == 0) {
+      res.status(400).json({
+        error: "User does not exist, sign up instead",
+      });
+    }else{
+        bcrypt.compare(password, userData[0].password, (err, result) => {
+          if(err){
+            res.status(500).json({
+              error: "Server error",
+            });
+          }else if(result == true){
+            const token = jwt.sign(
+              {
+                email: email,
+              },
+              process.env.secret_key
+            );
+            res.status(200).json({
+              message: "User signed in successfully",
+              token: token,
+            });
+          }else{
+            res.status(400).json({
+              error: "Enter correct password",
+            });
+          }
+        });
+      }
+      
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Database error occurred",
+      });
+    });
 };
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const client = require("./configs/db");
 // const postRoutes = require('./routes/posts');
-require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 8000;
+
+client.connect((err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log("Connected to database");
+});
 
 app.get('/', (req, res) => {
     res.status(200).send(`Server is up and running`);
@@ -20,4 +29,3 @@ app.use("/auth" , authRoutes);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
-
