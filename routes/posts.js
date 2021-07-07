b15826d2695f69a75ee52733f09541359fb3ca84
arushi2715 <arushiagr1720@gmail.com>
2021-07-08 01:17:29 +0530

    routes almost done

--- routes/posts.js
+++ routes/posts.js
@@ -15,8 +15,6 @@ router.param("postId", postIdParam);
 
 router.post("/create", verifyToken, createPost);
 
-router.post("/uploadimage", uploadImage); //add verifyToken
-
 router.get("/getposts", verifyToken, getPosts);
 
 router.put("/update/:postId", verifyToken, updatePosts);
@@ -31,5 +29,5 @@ module.exports = router;
 //localhost:8000/posts/uploadimage
 // localhost:8000/posts/update/:postId
 // localhost:8000/posts/delete/:postId
-// localhost:8000/posts/getuserposts
+// localhost:8000/posts/getposts
 // localhost:8000/posts/getallposts
