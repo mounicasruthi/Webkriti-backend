d606c3f4ecc15b56e8c28a77c2e8bf1cafe50f80
mounicasruthi <mounicasruthi30@gmail.com>
2021-07-09 00:02:25 +0530

    modified db.js

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
 
-const client = new Client({connectionString: process.env.DB_URL, ssl: {rejectUnauthorized: false} });
+const client = new Client({connectionString: process.env.DB_URL, ssl: true });
 
 module.exports = client;
